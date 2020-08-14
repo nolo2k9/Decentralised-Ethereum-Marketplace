c7b5aed3973fe3940405da3c6abe5ab6200d0da8
Keith Nolan <g00351932@gmit.ie>
2020-08-14 17:09:56 +0100

    got project started conneted smart contract to the blockchain

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0; 
+
+contract Marketplace {
+    //declaring state variables
+    string public name;   
+
+    constructor() public {
+        name = "ETH Marketplace";
+        
+    }
+}
