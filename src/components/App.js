811575d1a0abfa1a9468e8f198e3e836cff1529c
Keith Nolan <g00351932@gmit.ie>
2020-08-18 19:17:49 +0100

    Hooked up smart contrats to front-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,85 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import logo from '../logo.png'
+import './App.css'
+import Marketplace from '../abis/Marketplace.json'
 
 class App extends Component {
-    //runs when load web3 is created
-    async componentWillMount(){
-        await this.loadWeb3()
-        //load
-        await this.loadBlockchainData()
+  componentDidMount() {
+    document.title = 'Blockchain Marketplace'
+  }
+  //runs when load web3 is created
+  async componentWillMount() {
+    await this.loadWeb3()
+    //load
+    await this.loadBlockchainData()
+  }
 
+  async loadWeb3() {
+    if (window.ethereum) {
+      //connect to blockchain
+      window.web3 = new Web3(window.ethereum)
+      //enable
+      await window.ethereum.enable()
     }
-
-    async loadWeb3() {
-        if (window.ethereum) {
-            //connect to blockchain
-          window.web3 = new Web3(window.ethereum)
-          //enable
-          await window.ethereum.enable()
-        }
-        //legacy broswer
-        else if (window.web3) {
-          window.web3 = new Web3(window.web3.currentProvider)
-        }
-        //if neither exist 
-        else {
-          window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-        }
-      }
-      //load accounts from blockchain
-      async loadBlockchainData(){
-        const web3 = window.web3
-        //load account
-        const accounts = await web3.eth.getAccounts()
-        this.setState({ account: accounts[0] })
-      }
-      constructor(props){
-          super(props)
-          this.state = {
-              account: ''
-          }
-      }
+    //legacy broswer
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    //if neither exist
+    else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider trying MetaMask!',
+      )
+    }
+  }
+  //load accounts from blockchain
+  async loadBlockchainData() {
+    const web3 = window.web3
+    //load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    //logging abi and network address
+    const networkid = await web3.eth.net.getId()
+    const networkData = Marketplace.networks[networkid]
+    if (networkData) {
+      const marketplace = new web3.eth.Contract(
+        Marketplace.abi,
+        networkData.address,
+      )
+      console.log(networkid)
+      console.log(marketplace)
+    } else {
+      window.alert('Marketplace contract not deployed to a dedicated networka')
+    }
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true,
+    }
+  }
 
   render() {
     return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-          >
-            Blockchain Marketplace
-          </a>
+          <small className="text-white">
+            <a>
+              <b>Blockchain Marketplace</b>
+            </a>
+          </small>
           <ul className="navbar-nav px-3">
-              <li className="nav-item text-nowrap d-none d-smnone d-sm-block">
-                  <small className="text-white"><span id="account"><b>ACCOUNT:</b>   {this.state.account}</span></small>
-              </li>
+            <li className="nav-item text-nowrap d-none d-smnone d-sm-block">
+              <small className="text-white">
+                <span id="account">
+                  <b>ACCOUNT:</b> {this.state.account}
+                </span>
+              </small>
+            </li>
           </ul>
         </nav>
         <div className="container-fluid mt-5">
@@ -68,16 +94,13 @@ class App extends Component {
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
                 <h1></h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
               </div>
             </main>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
