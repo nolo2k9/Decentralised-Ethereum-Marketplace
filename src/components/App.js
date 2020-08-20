e2fe7983ef8b66322d74bc9f50804f6edb705941
Keith Nolan <g00351932@gmit.ie>
2020-08-20 13:29:31 +0100

    Added functionality to create a product on the blockchain from front-end

--- src/components/App.js
+++ src/components/App.js
@@ -51,9 +51,9 @@ class App extends Component {
         networkData.address,
       )
       this.setState({ marketplace: marketplace })
+      const productCount = await marketplace.methods.productCount().call()
+      console.log(productCount.toString())
       this.setState({ loading: false })
-      console.log(networkid)
-      console.log(marketplace)
     } else {
       window.alert('Marketplace contract not deployed to a dedicated networka')
     }
@@ -66,6 +66,13 @@ class App extends Component {
       products: [],
       loading: true,
     }
+    this.createProduct = this.createProduct.bind(this)
+  }
+  createProduct(name, price){
+    this.setState({loading: true})
+    this.state.marketplace.methods.createProduct(name, price).send({from: this.state.account}).once('receipt', (receipt)=>{
+      this.setState({loading: false})
+    })
   }
   render() {
     return (
@@ -74,7 +81,8 @@ class App extends Component {
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              {this.state.loading ? <div id ="loader" className="text-center"><p className = "text-center"> Loading...</p> </div> : <Main/> }
+              {this.state.loading ? <div id ="loader" className="text-center"><p className = "text-center"> Loading...</p> </div> 
+              : <Main createProduct={this.createProduct}/> }
             </main>
           </div>
         </div>
